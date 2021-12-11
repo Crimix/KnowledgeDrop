b6163d4eb2656d83e1a4f6d9eec7c9a7e6588a74
crimix <christofferm@live.dk>
2021-12-11 14:17:22 +0100

    Port to 1.18

--- src/main/java/com/black_dog20/knowledgedrop/KnowledgeDrop.java
+++ src/main/java/com/black_dog20/knowledgedrop/KnowledgeDrop.java
@@ -9,8 +9,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.fmllegacy.network.NetworkRegistry;
-import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- src/main/java/com/black_dog20/knowledgedrop/common/enchantments/ModEnchantments.java
+++ src/main/java/com/black_dog20/knowledgedrop/common/enchantments/ModEnchantments.java
@@ -4,9 +4,9 @@ import com.black_dog20.knowledgedrop.KnowledgeDrop;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModEnchantments {
 
